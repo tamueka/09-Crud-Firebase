05c4ba8292934af015073a44646c20ea4f4c87af
tamueka <tamueka@hotmail.com>
2020-04-19 16:07:57 +0200

    creamos enrutado de la app + enlazamos librerias bootstrap fontawesome y animate.css + creamos los componentes de la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeroeComponent, HeroesComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
