0b1a5537890c0c49c5f053d85cb4d6d2341de460
tamueka <tamueka@hotmail.com>
2020-04-19 17:42:12 +0200

    modelo para manejar heroes y formulario de edicion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
@@ -10,7 +11,7 @@ import { HeroesComponent } from './pages/heroes/heroes.component';
 
 @NgModule({
   declarations: [AppComponent, HeroeComponent, HeroesComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
