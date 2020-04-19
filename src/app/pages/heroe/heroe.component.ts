17495a3a5e9bda7fdefc34498d2287d2a4210a49
tamueka <tamueka@hotmail.com>
2020-04-19 19:21:50 +0200

    informacion del estado de las peticiones con sweetalert

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HeroeModel } from 'src/app/models/heroe.model';
 import { NgForm } from '@angular/forms';
 import { HeroesService } from 'src/app/services/heroes.service';
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroe',
@@ -22,19 +24,33 @@ export class HeroeComponent implements OnInit {
       return;
     }
 
-    if ( this.heroe.id ) {
-
-      this.heroesService.actualizarHeroe(this.heroe).subscribe( resp => {
-        console.log(resp)
-      })
-
-    } else {
-      this.heroesService.crearHeroe(this.heroe).subscribe( resp => {
-      console.log(resp);
-      this.heroe = resp;
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando informacion',
+      icon: 'info',
+      allowOutsideClick: false
     });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
+
+    if ( this.heroe.id ) {
+      peticion = this.heroesService.actualizarHeroe(this.heroe);
+    } else {
+      peticion = this.heroesService.crearHeroe(this.heroe);
     }
-  }
 
 
-}
+  peticion.subscribe(resp => {
+
+    Swal.fire({
+      title: 'this.heroe.nombre',
+      text: 'Se actualizo correctamente',
+      icon: 'success',
+    });
+
+  });
+
+}}
+
