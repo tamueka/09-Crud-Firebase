9b0dbd14a01bcde0faaa975c41f45c3c0bd73c1d
tamueka <tamueka@hotmail.com>
2020-04-20 17:04:28 +0200

    HTTP GET obtenemos un nodo especifico por id

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -4,6 +4,7 @@ import { NgForm } from '@angular/forms';
 import { HeroesService } from 'src/app/services/heroes.service';
 import Swal from 'sweetalert2';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
@@ -13,9 +14,22 @@ import { Observable } from 'rxjs';
 export class HeroeComponent implements OnInit {
   heroe: HeroeModel = new HeroeModel();
 
-  constructor(private heroesService: HeroesService) {}
+  constructor(private heroesService: HeroesService,
+              private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+    const id = this.route.snapshot.paramMap.get('id')
+
+    if ( id !== 'nuevo' ){
+      this.heroesService.getHeroe( id )
+      .subscribe( (resp: HeroeModel) => {
+        this.heroe = resp;
+        this.heroe.id = id;
+      })
+    }
+
+  }
 
   guardar(form: NgForm) {
 
